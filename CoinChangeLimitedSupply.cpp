5c3ca346ee8e720bfe65a8fa65650a95467ef7ea
S.M. Nabiul Islam <129619658+NabiulIslamNabil@users.noreply.github.com>
2024-10-08 02:40:56 +0600

    Add files via upload

---
+++ CoinChangeLimitedSupply.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int coins[n], supply[n];
+    for(int i=0; i<n; i++){
+        cin>>coins[i];
+        cin>>supply[i];
+    }
+
+    int change;
+    cin>>change;
+    
+    int dp[n][change+1];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=change; j++){
+            dp[i][j] = 0;
+        }
+    }
+
+    dp[0][0] = 1;
+    for(int i=1; i<n; i++){
+        for(int j=0; j<=change; j++){
+            dp[i][j] = dp[i-1][j];
+            
+            for(int k=1; k<=supply[i-1] && k*coins[i-1]<=j; k++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-k*coins[i-1]];
+            }
+        }
+    }
+
+    //print dp table
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=change; j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"Possible ways: "<<dp[n-1][change];
+}
